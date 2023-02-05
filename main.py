aec2b3698151c5d46f97bc54cd0b8862b012d184
Luca Silviu-Catalin <catalinluca01@gmail.com>
2023-02-06 01:41:38 +0200

    add main functionality

--- main.py
+++ main.py
@@ -1,9 +1,107 @@
+#/bin/python3
+# mpiexec --oversubscribe -n $PROC_NUMBER python3 main.py <input_dir> <output_dir>
+
 from mpi4py import MPI
+from utils import *
+import sys
+import numpy as np
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
+COMM_TAG = 99
 
-if rank == 0:
-    print("Hello World from process", rank)
+# CONFIG
+MASTER_NODE   = 1
+MAPPER_NODES  = [2, 3, 4]
+REDUCER_NODES = [5, 6]
+
+#PRECALCULATION
+ROOT_DIR = os.path.abspath(os.getcwd()) + '/'
+DUMP_DIR = ROOT_DIR + "dump"
+MAPPING_DUMP_DIR  = DUMP_DIR + "/input_mapping_dump"
+MAPPER_RESULT_DIR = DUMP_DIR + "/mapper_result_dump"
+REDUCER_DATA_DIR  = DUMP_DIR + "/reducer_input_dataset"
+RESULT_DUMP_DIR   = DUMP_DIR + "/result_dump"
+
+DUMPS_DIRS = [ MAPPING_DUMP_DIR, MAPPER_RESULT_DIR, REDUCER_DATA_DIR, RESULT_DUMP_DIR ]
+
+if (len(sys.argv) != 3):
+    if (rank == 1):
+        print_usage()
 else:
-    print("Greeting from process", rank)
+    input_dir  = ROOT_DIR + sys.argv[1]
+    output_dir = ROOT_DIR + sys.argv[2]
+    
+    if rank is MASTER_NODE:
+        # INIT
+        cleanup_dump(DUMP_DIR)
+
+        for dir in DUMPS_DIRS:
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+        # ASIGN MAPPER WORKLOAD
+        input_file_names = os.listdir(input_dir)
+        planned_workload = asign_mappers_workload(input_dir, MAPPER_NODES)
+
+        for mapper, workload in planned_workload.items():
+            comm.send(workload, dest=mapper, tag=COMM_TAG)
+
+        # GET MAPPER RESULTS
+        files = []
+        for mapper in MAPPER_NODES:
+            result_file = comm.recv(source=MPI.ANY_SOURCE, tag=COMM_TAG)
+            files.append(result_file)
+
+        # ASIGN REDUCER WORKLOAD
+        for reducer in REDUCER_NODES:
+            comm.send(files, reducer, tag=COMM_TAG)
+
+        # WAIT FOR PROCESSING
+        for reducer in REDUCER_NODES:
+            comm.recv(source=MPI.ANY_SOURCE, tag=COMM_TAG)
+
+        print ("ALL GOOD!")
+        print ("Doamne ajuta!")
+
+    elif rank in MAPPER_NODES:
+        response = {}
+        files = comm.recv(source=MASTER_NODE, tag=COMM_TAG)
+        for file_name in files:
+            file_path = input_dir + "/" + file_name
+            word_map = compute_word_map(file_path)
+
+            name, ext = os.path.splitext(file_name)
+            dump_dict(word_map, MAPPING_DUMP_DIR + "/" + name + "_word-map.json")
+            
+            response = merge_dict(response, invert_dict(word_map, file_name))
+        
+        result_dump_path = MAPPER_RESULT_DIR + "/" + str(rank) + "_result.json"
+        dump_dict(response, result_dump_path)
+
+        comm.send(result_dump_path, dest=MASTER_NODE, tag=COMM_TAG)
+
+    elif rank in REDUCER_NODES:
+        files = comm.recv(source=MPI.ANY_SOURCE, tag=COMM_TAG)
+        
+        working_dict = {}
+        for file_path in files:
+            working_dict = merge_dict(working_dict, load_dict(file_path))
+        
+        if rank == REDUCER_NODES[0]:
+            dump_dict(working_dict, REDUCER_DATA_DIR + "/reducer_dataset.json")
+    
+        to_do_letters = get_reducer_letters(rank, REDUCER_NODES)
+        log_msg = str(rank) + " -> " + str(to_do_letters) + "\n"
+        dump_string(log_msg, REDUCER_DATA_DIR + "/reducers_letters.log")
+
+        results = {}
+        for word, files in working_dict.items():
+            if any(word.startswith(letter) for letter in to_do_letters):
+                result_files = dict(sorted(files.items(), key=lambda item: item[1], reverse=True))
+                results[word] = result_files
+        
+        dump_dict(results, RESULT_DUMP_DIR + "/results_" + str(rank) + ".json")
+
+        comm.send(True, MASTER_NODE, tag=COMM_TAG)
---
+++ utils.py
@@ -0,0 +1,103 @@
+import os
+import re
+import chardet
+import json
+import shutil
+import numpy as np
+
+ENGLISH_LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def print_usage():
+    print("[ERROR] bad usage")
+    print("usage:   mpiexec -n [processes number] python3 main.py [input_dir] [output_dir")
+    print("example: mpiexec -n 9 python3 main.py test-files output_dir")
+
+
+def cleanup_dump(dirpath):
+    if os.path.exists(dirpath):
+        shutil.rmtree(dirpath)
+
+def asign_mappers_workload(input_dir, mapper_nodes):
+    file_names = os.listdir(input_dir)
+    total_size = 0
+    file_size = {}
+
+    for file_name in file_names:
+        file_path = input_dir + "/" +  file_name
+        current_file_size = os.path.getsize(file_path)
+        total_size += current_file_size
+        file_size[file_name] = current_file_size
+
+    target_workload = total_size / len(mapper_nodes)
+
+    response = {}
+    mapper_load = {}
+    for mapper in mapper_nodes:
+        mapper_load[mapper] = 0
+        response[mapper] = []
+
+    sorted_file_size = sorted(file_size.items(), key=lambda item: item[1], reverse=True)
+    for fname, fsize in sorted_file_size:
+        mapper = min(mapper_load.items(), key=lambda x: x[1])[0]
+        mapper_load[mapper] += fsize
+        response[mapper].append(fname)
+
+    return response
+
+
+def read_file(filename):
+    with open(filename, 'rb') as f:
+        result = chardet.detect(f.read())
+        encoding = result['encoding']
+    with open(filename, 'r', encoding=encoding) as f:
+        text = f.read()
+    # process the text as desired
+    return text
+
+def compute_word_map(file_path):
+    word_map = {}
+    contents = read_file(file_path)
+    words = re.findall(r'\b\w+\b', contents)
+    for word in words:
+        word = word.lower()
+        if word in word_map:
+            word_map[word] += 1
+        else:
+            word_map[word] = 1
+
+    return word_map
+
+def dump_dict(data, file_path):
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4)
+
+def dump_string(data, file_path):
+    with open(file_path, "a") as file:
+        file.write(data)
+    
+def load_dict(file_path):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return data
+
+def invert_dict(word_map, file_name):
+    result = {}
+    for word, count in word_map.items():
+        if word not in result:
+            result[word] = {file_name: count}
+        else:
+            result[word][file_name] += count
+    return result
+
+def merge_dict(a, b):
+    result = {**a, **b}
+    for key, value in result.items():
+        if key in a and key in b:
+            result[key] = {**a[key], **b[key]}
+    return result
+
+def get_reducer_letters(rank, reducers):
+    index = reducers.index(rank)
+    letter_chunks = np.array_split(ENGLISH_LETTERS, len(reducers))
+    
+    return letter_chunks[index]
